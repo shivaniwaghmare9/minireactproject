a8a551c1e76be8b719db1d666df386feea72c268
shivani <shivaniw69@gmail.com>
2025-06-26 14:47:49 +0530

    app

--- todoApp/src/App.jsx
+++ todoApp/src/App.jsx
@@ -1,8 +1,25 @@
 
+import { useSelector,useDispatch } from "react-redux";
+import { addTask } from "./todoSlice";
+import { useState } from "react";
 const App=()=>{
+  const data=useSelector(state=>state.todo.task);
+  const dispatch=useDispatch();
+  const[txt,setTxt]=useState("");
+  const ans=data.map((key)=>{
+    return(
+      <>
+       <tr>
+        
+       </tr>
+      </>
+    )
+  })
   return(
     <>
      <h1>Todo App!!</h1>
+     Enter Task: <input type="text" value={txt} onChange={(e)=>{setTxt(e.target.value)}}/><br/><br/>
+     <button onClick={()=>{dispatch(addTask({work:txt}))}}>Add</button>
     </>
   )
 }
