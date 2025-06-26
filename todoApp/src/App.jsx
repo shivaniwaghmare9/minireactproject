5dae059c62a6f4f731794197d0075df65bba02ab
shivani <shivaniw69@gmail.com>
2025-06-26 14:53:27 +0530

    table

--- todoApp/src/App.jsx
+++ todoApp/src/App.jsx
@@ -1,4 +1,5 @@
 
+import Table from 'react-bootstrap/Table';
 import { useSelector,useDispatch } from "react-redux";
 import { addTask } from "./todoSlice";
 import { useState } from "react";
@@ -6,11 +7,14 @@ const App=()=>{
   const data=useSelector(state=>state.todo.task);
   const dispatch=useDispatch();
   const[txt,setTxt]=useState("");
+  let sno=0;
   const ans=data.map((key)=>{
+    sno++;
     return(
       <>
        <tr>
-        
+          <td>{sno}</td>
+          <td>{key.work}</td>
        </tr>
       </>
     )
@@ -19,7 +23,19 @@ const App=()=>{
     <>
      <h1>Todo App!!</h1>
      Enter Task: <input type="text" value={txt} onChange={(e)=>{setTxt(e.target.value)}}/><br/><br/>
-     <button onClick={()=>{dispatch(addTask({work:txt}))}}>Add</button>
+     <button onClick={()=>{dispatch(addTask({work:txt}))}}>Add</button><br/><br/>
+     <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>Sno</th>
+          <th>Task</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+         {ans}
+      </tbody>
+      </Table>
     </>
   )
 }
