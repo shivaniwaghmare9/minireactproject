f4f2424f25cd6d9c544eca17e5f6708ddcd5a689
shivani <shivaniw69@gmail.com>
2025-07-02 00:35:28 +0530

    card

--- shoppingapp/src/pages/Display.jsx
+++ shoppingapp/src/pages/Display.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 const Display=()=>{
     const[mydata,setMydata]=useState([]);
     const loadData=async()=>{
@@ -15,12 +17,27 @@ const Display=()=>{
     const ans=mydata.map((key)=>{
         return(
             <>
+    <Card style={{ width: '14rem' }} id="card">
+      <Card.Body>
+        <Card.Title><h4>Product Name:{key.productName}</h4></Card.Title>
+        <Card.Text>
+                <h6>Id:{key.serialId}</h6>
+                <h6>Brandname:{key.brandName}</h6>
+                <h6>Price:{key.price}</h6>
+                <h6>Colo:r{key.color}</h6>
+                <h6>Size:{key.size}</h6>
+        </Card.Text>
+        <Button variant="primary" id="cardbtn" onClick={()=>{dispatch(addCart({id:key.id, productName:key.productName, brandName:key.brandName,price:key.price, color:key.color,key:key.size}))}}>Add cart</Button>
+      </Card.Body>
+    </Card>
             </>
         )
     })
     return(
         <>
-         <h3>display</h3>
+         <div>
+            {ans}
+         </div>
         </>
     )
 }
