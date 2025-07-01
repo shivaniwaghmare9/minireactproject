b1fbdd8ca960340cac6e182655181bd6cee5f620
shivani <shivaniw69@gmail.com>
2025-07-02 00:19:34 +0530

    navbar

---
+++ shoppingapp/src/Shopping.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+const Shopping=()=>{
+    const[cart,setCart]=useState("");
+    
+    return(
+        <>
+        <h3>Shopping App!!!</h3>
+        </>
+    )
+}
+export default Shopping;
