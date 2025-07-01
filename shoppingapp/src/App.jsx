128be95d932c9cc26fe967736a317abd361fc600
shivani <shivaniw69@gmail.com>
2025-07-02 00:30:35 +0530

    display

--- shoppingapp/src/App.jsx
+++ shoppingapp/src/App.jsx
@@ -1,13 +1,14 @@
 
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Layout from "./Layout";
+import Display from "./pages/Display";
 const App=()=>{
   return(
     <>
      <BrowserRouter>
        <Routes>
           <Route path="/" element={<Layout/>}>
-
+          <Route index element={<Display/>}/>
           </Route>
        </Routes>
      </BrowserRouter>
--- shoppingapp/src/pages/Display.jsx
+++ shoppingapp/src/pages/Display.jsx
@@ -1,8 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Display=()=>{
+    const[mydata,setMydata]=useState([]);
+    const loadData=async()=>{
+        let api="https://mocki.io/v1/10512a17-105f-435d-a920-dce1478345bd"
+        const response=await axios.get(api)
+        console.log(response.data);
+        setMydata(response.data);
+    }
+    useEffect(()=>{
+        loadData()
+    },[])
     return(
         <>
-         
+         <h3>display</h3>
         </>
     )
 }
