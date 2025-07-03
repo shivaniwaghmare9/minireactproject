fddec5a70e44175c3f0dd279809adb6524402456
shivani <shivaniw69@gmail.com>
2025-07-04 01:55:56 +0530

    flip

--- coinapp/src/App.jsx
+++ coinapp/src/App.jsx
@@ -12,7 +12,7 @@
 import { flipcount } from "./coinSlice";
 import { useSelector,useDispatch } from "react-redux";
 const App=()=>{
-    const {coin,count,headcount,tailcount}=useSelector(state=>state.mycoin)
+    const {coin,count,headscount,tailscount}=useSelector(state=>state.mycoin)
     const dispatch=useDispatch();
     return(
         <>
@@ -20,8 +20,8 @@ const App=()=>{
         <h3>Flip Coin</h3>
         <h4>{coin}</h4>
         <h4>TotalCount:{count}</h4>
-        <h4>HeadsCount:{headcount}</h4>
-        <h4>TailsCount:{tailcount}</h4>
+        <h4>HeadsCount:{headscount}</h4>
+        <h4>TailsCount:{tailscount}</h4>
         <button onClick={()=>{dispatch(flipcount())}}>Flip</button>
         </div>
          
