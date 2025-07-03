8b17fface3b6ee9e12644dc3d291471eb26f8358
shivani <shivaniw69@gmail.com>
2025-07-03 21:54:03 +0530

    coin

--- coinapp/src/App.jsx
+++ coinapp/src/App.jsx
@@ -2,13 +2,23 @@
 import { useState } from "react";
 
 const App=()=>{
-    const[coin,setCoin]=useState(0);
+    const[coin,setCoin]=useState("Heads")
     const[flipcount,setFlipcount]=useState(0)
     const[heads,setHeads]=useState(0)
     const[tails,setTails]=useState(0)
 
     const flipcoin=()=>{
-          
+           const flip=Math.random() < 0.5 ? "Heads" : "Tails";
+           setCoin(flip);
+           setFlipcount(flipcount+1);
+           if(flip === "Heads")
+           {
+               setHeads(heads+1)
+           }
+           else
+           {
+               setTails(tails+1)
+           }
     }
     return(
         <>
