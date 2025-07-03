d6f65881d9399b7000524c188c1613b0a3c79fb6
shivani <shivaniw69@gmail.com>
2025-07-03 21:46:58 +0530

    app

---
+++ coinapp/src/App.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from "react";
+
+const App=()=>{
+    const[coin,setCoin]=useState(0);
+    const[flipcount,setFlipcount]=useState(0)
+    const[heads,setHeads]=useState(0)
+    const[tails,setTails]=useState(0)
+
+    const flipcoin=()=>{
+          
+    }
+    return(
+        <>
+        <h3>flip coin</h3>
+        <h4></h4>
+
+        </>
+    )
+}
+export default App;
