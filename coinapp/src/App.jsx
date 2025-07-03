9de2b38cd8d04fc8071ff2f53f348a2de33f42a1
shivani <shivaniw69@gmail.com>
2025-07-04 02:20:16 +0530

    css

--- coinapp/src/App.jsx
+++ coinapp/src/App.jsx
@@ -17,14 +17,16 @@ const App=()=>{
     return(
         <>
         <div id="counter">
-        <h3>Flip Coin</h3>
+        <h2><span style={{color:"darkred"}}>FL</span>IP  <span style={{color:"darkred"}}>C</span>OIN</h2>
+        <div className="coin">
         <h4>{coin}</h4>
-        <div className="count">
-        <h4>TotalCount:{count}</h4>
-        <h4>HeadsCount:{headscount}</h4>
-        <h4>TailsCount:{tailscount}</h4>
         </div>
-        <button onClick={()=>{dispatch(flipcount())}}>Flip</button>
+        <div className="count">
+        <h4 id="btn1">TotalCount:{count}</h4>
+        <h4 id="btn2">HeadsCount:{headscount}</h4>
+        <h4 id="btn3">TailsCount:{tailscount}</h4>
+        </div>
+        <button onClick={()=>{dispatch(flipcount())}} className="btn">FlipCoin</button>
         </div>
          
         </>
