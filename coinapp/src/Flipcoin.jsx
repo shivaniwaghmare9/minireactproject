bc677fa73bd7cf87cf22a5bdc332fbd84ec9f362
shivani <shivaniw69@gmail.com>
2025-07-22 22:07:54 +0530

    flip

--- coinapp/src/Flipcoin.jsx
+++ coinapp/src/Flipcoin.jsx
@@ -1,5 +1,41 @@
 
 
+// import { useState } from "react";
+
+// const Flip=()=>{
+//     const[coin,setCoin]=useState("Heads")
+//     const[flipcount,setFlipcount]=useState(0)
+//     const[heads,setHeads]=useState(0)
+//     const[tails,setTails]=useState(0)
+
+//     const flipcoin=()=>{
+//            const flip=Math.random() < 0.5 ? "Heads" : "Tails";
+//            setCoin(flip);
+//            setFlipcount(flipcount+1);
+//            if(flip === "Heads")
+//            {
+//                setHeads(heads+1)
+//            }
+//            else
+//            {
+//                setTails(tails+1)
+//            }
+//     }
+//     return(
+//     <>
+        
+//         <h4>{coin}</h4>
+//         <p>Flipcount:{flipcount}</p>
+//         <p>Heads:{heads}</p>
+//         <p>Tails:{tails}</p><br/>
+//         <button onClick={flipcoin}>Click</button>
+//     </>
+//     )
+// }
+// export default Flip;
+
+
+
 import { useState } from "react";
 
 const Flip=()=>{
