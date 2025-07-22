4c90f5b092c4abd15b2b9183fe6fafe112475dd8
shivani <shivaniw69@gmail.com>
2025-07-22 22:17:56 +0530

    app

--- shoppingapp/src/App.jsx
+++ shoppingapp/src/App.jsx
@@ -1,4 +1,28 @@
 
+// import { BrowserRouter,Route,Routes } from "react-router-dom";
+// import Layout from "./Layout";
+// import Display from "./pages/Display";
+// import Sorting from "./pages/Sorting";
+// import Search from "./pages/Search";
+// const App=()=>{
+//   return(
+//     <>
+//      <BrowserRouter>
+//        <Routes>
+//           <Route path="/" element={<Layout/>}>
+//           <Route index element={<Display/>}/>
+//           <Route path="display" element={<Display/>}/>
+//           <Route path="sort" element={<Sorting/>}/>
+//           <Route path="search" element={<Search/>}/>
+//           </Route>
+//        </Routes>
+//      </BrowserRouter>
+//     </>
+//   )
+// }
+// export default App;
+
+
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Display from "./pages/Display";
