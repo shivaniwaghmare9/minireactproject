7886f467960665906b537a373ac89e45737dbe0b
shivani <shivaniw69@gmail.com>
2025-07-22 22:06:07 +0530

    app

--- coinapp/src/App.jsx
+++ coinapp/src/App.jsx
@@ -33,3 +33,39 @@ const App=()=>{
     )
 }
 export default App;
+
+// import Flip from "./Flipcoin";
+// const App=()=>{
+//     return(
+//     <>
+//     <h3>Flip Coin</h3>
+//     <Flip/>
+//     </>
+//     )
+// }
+// export default App;
+
+import { flipcount } from "./coinSlice";
+import { useSelector,useDispatch } from "react-redux";
+const App=()=>{
+    const {coin,count,headscount,tailscount}=useSelector(state=>state.mycoin)
+    const dispatch=useDispatch();
+    return(
+        <>
+        <div id="counter">
+        <h2 style={{textDecoration:"underline"}}><span style={{color:"darkred"}}>FL</span>IP  <span style={{color:"darkred"}}>CO</span>IN</h2>
+        <div className="coin">
+        <h4>{coin}</h4>
+        </div>
+        <div className="count">
+        <h4 id="btn1">TotalCount:{count}</h4>
+        <h4 id="btn2">HeadsCount:{headscount}</h4>
+        <h4 id="btn3">TailsCount:{tailscount}</h4>
+        </div>
+        <button onClick={()=>{dispatch(flipcount())}} className="btn">FlipCoin</button>
+        </div>
+         
+        </>
+    )
+}
+export default App;
