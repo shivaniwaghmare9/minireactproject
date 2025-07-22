936b9b4a11c7fadd84c01bf7f3e16d0b5caa7833
shivani <shivaniw69@gmail.com>
2025-07-22 22:20:26 +0530

    nav

--- shoppingapp/src/Navbar.jsx
+++ shoppingapp/src/Navbar.jsx
@@ -1,4 +1,28 @@
 
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import { Link } from 'react-router-dom';
+// const Topnav=()=>{
+//     return(
+//         <>
+//          <Navbar bg="dark" data-bs-theme="dark">
+//         <Container>
+//           <Navbar.Brand href="#home">TASK</Navbar.Brand>
+//           <Nav className="me-auto">
+//             <Nav.Link as={Link} to="display">Display</Nav.Link>
+//             <Nav.Link as={Link} to="sort">Sorting</Nav.Link>
+//             <Nav.Link as={Link} to="search">Search</Nav.Link>
+            
+//           </Nav>
+//         </Container>
+//       </Navbar>
+//         </>
+//     )
+// }
+// export default Topnav;
+
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
