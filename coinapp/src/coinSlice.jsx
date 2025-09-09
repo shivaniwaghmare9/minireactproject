5c09cf139d816d12c7e901a5beea01aad617f5b5
shivani <shivaniw69@gmail.com>
2025-09-09 22:02:33 +0530

    slice

--- coinapp/src/coinSlice.jsx
+++ coinapp/src/coinSlice.jsx
@@ -1,3 +1,30 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const coinSlice=createSlice({
+//     name:"mycoin",
+//     initialState:{
+//         coin:"Heads",
+//         count:0,
+//         headscount:0,
+//         tailscount:0
+//     },
+//     reducers:{
+//         flipcount:(state)=>{
+//             state.coin=Math.random() < 0.5 ?"Heads":"Tails";
+//             state.count+=1;
+//             if(state.coin==="Heads")
+//             {
+//                 state.headscount+=1;
+//             }
+//             else
+//             {
+//                 state.tailscount+=1;
+//             }
+//         }
+//     }
+// })
+// export const {flipcount}=coinSlice.actions;
+// export default coinSlice.reducer;
 import { createSlice } from "@reduxjs/toolkit";
 
 const coinSlice=createSlice({
