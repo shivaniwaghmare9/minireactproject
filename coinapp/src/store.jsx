edf51a9eaa2958b90c725ab9f48ebcfa4ee59ac2
shivani <shivaniw69@gmail.com>
2025-09-09 22:05:32 +0530

    store

--- coinapp/src/store.jsx
+++ coinapp/src/store.jsx
@@ -1,11 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import coinReducer from "./coinSlice"
-// const store=configureStore({
-//     reducer:{
-//      mycoin:coinReducer
-//     }
-// })
-// export default store;
 import { configureStore } from "@reduxjs/toolkit";
 import coinReducer from "./coinSlice"
 const store=configureStore({
